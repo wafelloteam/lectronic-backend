bc78f6ea24326f45c68333f30e9da7f40e704a6f
RFauzi <119996191+rfauzi44@users.noreply.github.com>
2023-03-02 14:13:02 +0700

    omit empty several fields for auth

--- database/orm/model/user.go
+++ database/orm/model/user.go
@@ -5,14 +5,14 @@ import (
 )
 
 type User struct {
-	ID      string     `gorm:"type:uuid; primaryKey; default:uuid_generate_v4()" json:"id,omitempty" valid:"-"`
+	ID          string     `gorm:"type:uuid; primaryKey; default:uuid_generate_v4()" json:"id,omitempty" valid:"-"`
 	FullName    string     `json:"full_name" valid:"type(string)"`
 	Email       string     `json:"email" valid:"email"`
 	Password    string     `json:"password,omitempty" valid:"type(string)"`
-	Gender      string     `json:"gender" valid:"type(string)"`
+	Gender      string     `json:"gender,,omitempty" valid:"-"`
 	Address     string     `json:"address,omitempty" valid:"-"`
 	Birthday    *time.Time `json:"birthday,omitempty" valid:"-"`
-	PhoneNumber string     `json:"phone_number" valid:"type(string)"`
+	PhoneNumber string     `json:"phone_number,omitempty" valid:"-"`
 	Role        string     `gorm:"default:user" json:"role,omitempty" valid:"-"`
 	CreatedAt   time.Time  `json:"created_at" valid:"-"`
 	UpdatedAt   time.Time  `json:"updated_at,omitempty" valid:"-" `
